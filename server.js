baf67758e53e28ca07bf0f60d43e62fb6ec326f8
dantedunn <dantedunn@gmail.com>
2025-04-15 23:38:08 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = process.env.PORT || 8080
+
+const colors = [
+  { color: ['red', 'white', 'blue'] },
+  'green',
+  { shapes: ['circle', 'oval', 'square'] }
+]
+
+app.get('/colors', (req, res) => {
+  res.status(200).json(colors)
+})
+
+// sendFile will go here
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+app.listen(port)
+console.log('Server started at http://localhost:' + port)
